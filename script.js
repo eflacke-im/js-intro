56f5966377380463b3754f6dad6fad9d324e282b
Eirik Flacké <eirik@gjennestadvgs.no>
2026-02-22 23:19:56 +0100

    Add more exaples

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Dette er JavaScript-filen
 // Åpne nettleseren og trykk F12 → Console
 
-console.log("Scriptet er lastet");
+console.log("Script i egen fil fungerer også");
 
 // Finne elementer på siden
 let button = document.querySelector("#myButton");
@@ -17,4 +17,79 @@ button.addEventListener("click", function () {
     console.log("Knappen er trykket", clickCount, "ganger");
 
     output.textContent = "Antall klikk: " + clickCount;
+});
+
+// Fargeknapp
+let colorButton = document.querySelector("#colorButton");  
+  
+function getRandomColor() {  
+let red = Math.floor(Math.random() * 256);  
+let green = Math.floor(Math.random() * 256);  
+let blue = Math.floor(Math.random() * 256);  
+  
+return "rgb(" + red + "," + green + "," + blue + ")";  
+}  
+  
+colorButton.addEventListener("click", function () {  
+document.body.style.backgroundColor = getRandomColor();  
+});
+
+// Si hei
+let nameInput = document.querySelector("#nameInput");
+let greetButton = document.querySelector("#greetButton");
+
+greetButton.addEventListener("click", function () {
+    let name = nameInput.value;
+
+    if (name === "") {
+        outputName.textContent = "Du har ikke skrevet noe navn";
+    } else {
+        outputName.textContent = "Hei, " + name + "!";
+    }
+});
+
+// Poengteller
+let score = 0;
+
+let scoreDisplay = document.querySelector("#scoreDisplay");
+let addPoint = document.querySelector("#addPoint");
+let resetScore = document.querySelector("#resetScore");
+
+function updateScore() {
+    scoreDisplay.textContent = "Poeng: " + score;
+}
+
+addPoint.addEventListener("click", function () {
+    score = score + 1;
+    updateScore();
+});
+
+resetScore.addEventListener("click", function () {
+    score = 0;
+    updateScore();
+});
+
+// Spilliste
+let games = ["TornCity", "Cyberpunk 2077", "Skyrim", "The Witcher 3"];
+
+let gameList = document.querySelector("#gameList");
+let showGames = document.querySelector("#showGames");
+
+showGames.addEventListener("click", function () {
+
+    gameList.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = games[i];
+        gameList.appendChild(li);
+    }
+});
+
+// Modusbytte
+let darkModeButton = document.querySelector("#darkModeButton");
+
+darkModeButton.addEventListener("click", function () {
+    document.body.style.backgroundColor = "";
+    document.body.classList.toggle("dark-mode");
 });
